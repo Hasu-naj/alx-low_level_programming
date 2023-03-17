9c5375b179f3a548acb57353ac1a7a1323f7d1da
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-17 08:29:26 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	write(2,"and that peice of art is usful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
