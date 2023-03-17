80b6935b7c43889553664d94c34e2dd694bc1c1c
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-17 14:17:29 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	// Get the character to be written
+	char ch = 'a';
+  
+	// Write the Character to stdout
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+        putchar ('\n');
+	return (0);
+}
