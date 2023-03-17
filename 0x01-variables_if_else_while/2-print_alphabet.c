cf892b71531ae7140a68d9c3db35b83bb07f3ded
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-17 14:19:50 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	// Get the character to be written
+	/* Get the character to be written*/
 	char ch = 'a';
   
-	// Write the Character to stdout
+	/* Write the Character to stdout*/
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
