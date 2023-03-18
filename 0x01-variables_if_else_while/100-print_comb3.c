9a2d5f9c4fe29d98239ec5616b9e0e95c20400c3
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-17 17:27:30 -0700

    print combination 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,25 @@
  */
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 	int j = 0;
 
         for (i = 0; i <= 9; i++)
-        {
-		for ( j = 0; j <= 9; j++)
-		{
-			if (i != j && i < j)
+        	{
+			for ( j = 0; j <= 9; j++)
 			{
-                		putchar('0' + i);
-				putchar('0' + j);
+				if (i != j && i < j)
+				{
+                			putchar('0' + i);
+					putchar('0' + j);
+				}
+                		if (i != j && i < j && i != 8)
+                		{
+                        		putchar(',');
+                        		putchar(' ');
+                		}
 			}
-                	if (i != j && i < j && i != 8)
-                	{
-                        	putchar(',');
-                        	putchar(' ');
-                	}
-		}
-        }
+        	}
         putchar('\n');
         return (0);
 }
