c9ff9a41a51badf532f5479097ad0b7305a7b119
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-17 17:11:48 -0700

    print combination 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i = 0;
+	int j = 0;
+
+        for (i = 0; i <= 9; i++)
+        {
+		for ( j = 0; j <= 9; j++)
+		{
+			if (i != j && i < j)
+			{
+                		putchar('0' + i);
+				putchar('0' + j);
+			}
+                	if (i != j && i != 8 && j != 9)
+                	{
+                        	putchar(',');
+                        	putchar(' ');
+                	}
+		}
+        }
+        putchar('\n');
+        return (0);
+}
