84b3fd35e8af37be41d35ca29867d13070e01ba8
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-21 13:38:13 -0700

    int _print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_sign - prints the sign of a number
+ * @n: n is parameter of integer type
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+		_putchar(43);
+	if else (n = 0)
+		return (0);
+		_putchar(48);
+	else
+		return(-1);
+		_putchar(45);
+}
