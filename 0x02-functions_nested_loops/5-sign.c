8736b3b29d70d6dab59a37f94f3f27c60c32ffc8
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-21 13:41:01 -0700

    int _print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,18 @@
 int _print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
 		_putchar(43);
+	}
 	if else (n = 0)
+	{
 		return (0);
 		_putchar(48);
+	}
 	else
+	{
 		return(-1);
 		_putchar(45);
+	}
 }
