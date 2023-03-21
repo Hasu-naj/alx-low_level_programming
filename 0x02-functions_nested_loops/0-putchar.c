323dfb79d11565403c19a86f8e50c0c9faebea6c
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-21 05:04:59 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program is print _putchar'
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/REDME.md
@@ -0,0 +1,1 @@
+This is a REDME file for functions nested loops directory
