6ca3b67135714dd0a022161256cee2052c8fed4b
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-21 14:43:26 -0700

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _abs(int) -  the absolute value of an integer
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int _abs(int);
+{
+	int n;
+	
+	if (n < 0)
+		n = (-1) * n;
+	_putchar(n);
+	_putchar('\n');
+}
+
