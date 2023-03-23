cc3cf1199a87e18c9e96ef56149c5112d380b756
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-23 10:28:24 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	_putchar(48);
+	_putchar(49);
+	_putchar(50);
+	_putchar(51);
+	_putchar(52);
+	_putchar(53);
+	_putchar(54);
+	_putchar(55);
+	_putchar(56);
+	_putchar(57);
+	_putchar('\n');
+	return (0);
+}
