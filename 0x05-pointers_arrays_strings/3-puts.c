dba78cbb31221f0b8cfbf30e9274f326aafc601b
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-27 15:58:18 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		printf("%s", str[i]);
+	}
+	printf("\n");
+	return (0);
+}
