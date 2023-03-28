43cf7de38cbb7dadb14f589dcf7ace4beb699344
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-27 19:28:02 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,8 +10,18 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int n = 0;
 
+	while (src[n] != '\0')
+	{
+		n++;
+	}
 	for (i = 0 ; src[i] != '\0'; i++)
 		dest[i] = src[i];
+	while (i <= n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
