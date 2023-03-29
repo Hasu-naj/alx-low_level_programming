389e4b1f5121dd2e5bca2934ee8ed73d9fcffa14
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-29 15:42:33 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - appends the src string to the dest string
+ * @dest: string
+ * @src: string
+ *
+ * Return: Return a string dest that concatanate two strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length;
+	int j;
+
+	length = 0;
+	while (dest[length] !='\0')
+		length++;
+	for(j=0; src[j] !='\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+
+	return dest;
+}
