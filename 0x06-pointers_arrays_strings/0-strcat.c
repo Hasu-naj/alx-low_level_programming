1a7f137819d53408a1d8538260ca305da27b5cf4
Hasu-naj <hajarbouazza.7@gmail.com>
2023-03-29 15:47:13 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,12 +14,12 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	length = 0;
-	while (dest[length] !='\0')
+	while (dest[length] != '\0')
 		length++;
-	for(j=0; src[j] !='\0'; j++, length++)
+	for (j = 0; src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
 	}
 	dest[length] = '\0';
-	return dest;
+	return (dest);
 }
