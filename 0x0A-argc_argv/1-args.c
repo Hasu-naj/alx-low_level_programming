cf6aa571a29f738dfa4ae8aa17ef2b8b26b594e3
Hasu-naj <hajarbouazza.7@gmail.com>
2023-04-09 04:51:50 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,11 @@
 /**
  * main - print the number of arguments
  * @argc: number of arguments
- * @argv: array of argiments
+ * @argv:array of arguments
  *
  * Return: Always (0) success
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void) argv; /*ignore argv*/
 	printf("%d\n", argc - 1);
