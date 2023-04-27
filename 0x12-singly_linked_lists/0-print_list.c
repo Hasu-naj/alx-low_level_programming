eabb065784ec79fb6548aff8448f700530bd3efa
Hasu-naj <hajarbouazza.7@gmail.com>
2023-04-27 03:16:36 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,13 @@ size_t print_list(const list_t *h)
 	i = 0;
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		i++;
-
 		h = h->next;
+
+		i++
 	}
 
 	return (i);
